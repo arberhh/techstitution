74e44fba29cf9093665e963ecdbd67cbeaaff80a
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2017-06-07 21:47:09 +0200

    added gitignore file

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -3,7 +3,10 @@ from flask import Blueprint, render_template, request, redirect, url_for
 mod_main = Blueprint('main', __name__)
 
 @mod_main.route('/', methods=['GET'])
-
 def index():
+    return render_template('index.html')
 
+
+@mod_main.route('/login', methods=['GET'])
+def login():
     return render_template('login.html')
